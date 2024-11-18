95fb23476863b17e855b2a7eb2017cf0f1f65b8c
Your Name <rimonron.ad@gmail.com>
2024-11-18 11:26:51 +0600

    first commit

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,13 @@
+
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-6xl">Hello world!</div>,
+  },
+]);
+
+export default router;
